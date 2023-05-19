9577acd1276b96b820f65e30c623215c65a92a33
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-05-19 12:15:14 +0200

    Display chart

--- src/components/ExpensesChart.tsx
+++ src/components/ExpensesChart.tsx
@@ -1,6 +1,46 @@
-export default function ExpensesChart(): JSX.Element{
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend 
+} from "chart.js";
+
+import { Bar } from 'react-chartjs-2'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title, 
+    Tooltip, 
+    Legend
+);
+
+type dataType = Array<{day: string; amount: number;}>;
+
+export default function ExpensesChart (prop: { data: dataType }){
+    const { data } = prop;
+
+    const options = {
+
+    }
 
 
-    return <>Chart</>
+    const datasets = {
+        labels: data.map((dataPoint: {day: string; amount: number;} ) => dataPoint.day),
+        datasets:[
+            {
+                data: data.map((dataPOint: {day: string; amount: number} ) => dataPOint.amount)
+            }
+        ] 
+    }
     
-}
+    return (
+        <>
+        <Bar options={options} data={datasets}/>
+        </>
+    );
+} 
