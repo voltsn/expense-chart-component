7ae727bb5c7f8c8d7aa166a11114b573ac802bc6
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-05-19 15:00:12 +0200

    Add html files to the content array

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.tsx", "./.html"],
+  content: ["./src/**/*.tsx", "./*.html"],
   theme: {
     extend: {},
   },
