6b8442089d89a369dc2ddbfb2d84920e85acdec5
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-05-19 16:05:49 +0200

    Extend colorscheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./src/**/*.tsx", "./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brown": {
+          400: "#92857A",
+          900: "#382314",
+        },
+        "card": {
+          100: "#FFFBF6"
+        },
+        "cream": "#F8E9DD",
+        "cyan": "#76B5BC",
+        "red": {
+          200: "#EC755D",
+        }
+      }
+    },
   },
   plugins: [],
 }
